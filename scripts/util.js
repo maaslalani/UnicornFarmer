00130f7f51044bdd9b4afc2b962a902865e43825
Maas Lalani <maaslalani1@gmail.com>
2018-12-20 08:29:40 -0500

    Add emoji on mouse click

--- scripts/util.js
+++ scripts/util.js
@@ -1,30 +1,31 @@
-const FRAMES = 24
+const FRAMES = 24;
+const RANDOMNESS = 25;
+const DELAY = 100;
 
 const SNACKS = [
-  new Snack({ name: 'Cookie', emoji: '🍪', cost: 100, efficiency: 1 }),
-  new Snack({ name: 'Popcorn', emoji: '🍿', cost: 1000, efficiency: 15 }),
-  new Snack({ name: 'Avocado', emoji: '🥑', cost: 2500, efficiency: 60 }),
-  new Snack({ name: 'Pancakes', emoji: '🥞', cost: 5000, efficiency: 150 }),
-  new Snack({ name: 'Baguette', emoji: '🥖', cost: 10000, efficiency: 325 }),
-  new Snack({ name: 'Taco', emoji: '🌮', cost: 50000, efficiency: 750 }),
-  new Snack({ name: 'Bagel', emoji: '🥯', cost: 1000000, efficiency: 1500 }),
-  new Snack({ name: 'Milk', emoji: '🥛', cost: 2500000, efficiency: 5000 }),
+  new Snack({ name: 'Cookie', emoji: '🍪', cost: 10, efficiency: 0.1 }),
+  new Snack({ name: 'Popcorn', emoji: '🍿', cost: 100, efficiency: 1.25 }),
+  new Snack({ name: 'Avocado', emoji: '🥑', cost: 200, efficiency: 2.5 }),
+  new Snack({ name: 'Pancakes', emoji: '🥞', cost: 500, efficiency: 7.5 }),
+  new Snack({ name: 'Baguette', emoji: '🥖', cost: 1000, efficiency: 15 }),
+  new Snack({ name: 'Taco', emoji: '🌮', cost: 5000, efficiency: 75 }),
+  new Snack({ name: 'Bagel', emoji: '🥯', cost: 10000, efficiency: 150 }),
+  new Snack({ name: 'Milk', emoji: '🥛', cost: 250000, efficiency: 5000 }),
 ]
 
 const EQUIPMENT = [
-  new Equipment({ name: 'Floppy Disk', emoji: '💾', cost: 1000, autonomy: 1 }),
-  new Equipment({ name: 'Compact Disk', emoji: '💿', cost: 5000, autonomy: 5 }),
-  new Equipment({ name: 'Mouse', emoji: '🖱', cost: 10000, autonomy: 10 }),
-  new Equipment({ name: 'Keyboard', emoji: '⌨️', cost: 20000, autonomy: 20 }),
-  new Equipment({ name: 'Phone', emoji: '📱', cost: 50000, autonomy: 55 }),
-  new Equipment({ name: 'Laptop', emoji: '💻', cost: 75000, autonomy: 80 }),
-  new Equipment({ name: 'Monitor', emoji: '🖥', cost: 300000, autonomy: 350 }),
-  new Equipment({ name: 'Watch', emoji: '⌚️', cost: 1250000, autonomy: 1500 }),
+  new Equipment({ name: 'Floppy Disk', emoji: '💾', cost: 100, autonomy: 1 }),
+  new Equipment({ name: 'Compact Disk', emoji: '💿', cost: 500, autonomy: 5 }),
+  new Equipment({ name: 'Mouse', emoji: '🖱', cost: 1000, autonomy: 10 }),
+  new Equipment({ name: 'Keyboard', emoji: '⌨️', cost: 2000, autonomy: 20 }),
+  new Equipment({ name: 'Phone', emoji: '📱', cost: 5000, autonomy: 55 }),
+  new Equipment({ name: 'Laptop', emoji: '💻', cost: 7500, autonomy: 80 }),
+  new Equipment({ name: 'Monitor', emoji: '🖥', cost: 30000, autonomy: 350 }),
+  new Equipment({ name: 'Watch', emoji: '⌚️', cost: 125000, autonomy: 1500 }),
 ]
 
 const createUpgrade = upgrade => { 
   let element = document.createElement('div');
-
   element.classList.add('upgrade');
 
   element.addEventListener('click', () => player.purchase(upgrade))
@@ -39,15 +40,27 @@ const createUpgrade = upgrade => {
   return element
 }
 
-const emojify = emoji => {
+const emojify = (event, emoji) => {
   let element = document.createElement('div');
+
   element.classList.add('emoji');
-  element.innerHTML = `<p>${emoji}</p>`
+  element.innerHTML = emoji;
+
+  // Position emoji
+  let { clientX, clientY } = event
+  let { random } = Math
+
+  let x = clientX + (random() - 1) * RANDOMNESS
+  let y = clientY + (random() - 1) * RANDOMNESS
+
+  element.style.left = `${x}px`;
+  element.style.top = `${y}px`;
 
   // Add emoji
-  document.body.appendChild(element)
+  target.appendChild(element)
 
-  // Remove emoji after one second
-  setTimeout(() => element.parentNode.removeChild(element), 1000)
+  setTimeout(() => element.style.opacity = 0, DELAY);
 
+  // Remove emoji after half a second
+  setTimeout(() => element.parentNode.removeChild(element), 500)
 }
